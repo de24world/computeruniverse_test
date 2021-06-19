b02ab8a9d2340197e8d81df3912baa1292244bdb
HoSoo Lee <hosoo.lee@adesso.de>
2021-06-19 23:37:53 +0200

    inital folder

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const IndexPage = () => (
+    <Layout title="Home | Next.js + TypeScript Example">
+        <h1>Hello Next.js 👋</h1>
+        {/*  SEARCH FORM */}
+    </Layout>
+);
+
+export default IndexPage;
---
+++ pages/search.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const SearchPage = () => <Layout title="Home | Next.js + TypeScript Example"></Layout>;
+
+export default SearchPage;
