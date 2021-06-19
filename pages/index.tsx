7ed2f782c5d24b0661aba0e4078f6aff475aee99
HoSoo Lee <hosoo.lee@adesso.de>
2021-06-20 01:47:16 +0200

    install algoliasearch & next-instantsearch and setting with a sample api(env.local)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,65 @@
-import React from 'react';
-import Layout from '../components/Layout';
+import algoliasearch from "algoliasearch/lite";
+import { withInstantSearch } from "next-instantsearch";
+import {
+  Configure,
+  Highlight,
+  Hits,
+  Pagination,
+  RefinementList,
+  SearchBox,
+} from "react-instantsearch-dom";
 
-const IndexPage = () => (
-    <Layout title="Home | Next.js + TypeScript Example">
-        <h1>Hello Next.js 👋</h1>
-        {/*  SEARCH FORM */}
-    </Layout>
+const HitComponent = ({ hit }) => (
+  <div className="hit">
+    <div>
+      <div className="hit-picture">
+        <img src={`${hit.image}`} />
+      </div>
+    </div>
+    <div className="hit-content">
+      <div>
+        <Highlight attribute="name" hit={hit} />
+        <span> - ${hit.price}</span>
+        <span> - {hit.rating} stars</span>
+      </div>
+      <div className="hit-type">
+        <Highlight attribute="type" hit={hit} />
+      </div>
+      <div className="hit-description">
+        <Highlight attribute="description" hit={hit} />
+      </div>
+    </div>
+  </div>
 );
 
-export default IndexPage;
+function IndexPage() {
+  return (
+    <>
+      <Configure hitsPerPage={12} />
+      <header>
+        <SearchBox />
+      </header>
+      <main>
+        <div className="menu">
+          <RefinementList attribute="categories" />
+        </div>
+        <div className="results">
+          <Hits hitComponent={HitComponent} />
+        </div>
+      </main>
+      <footer>
+        <Pagination />
+      </footer>
+    </>
+  );
+}
+
+const searchClient = algoliasearch(
+  process.env.APPLICATION_ID,
+  process.env.API_KEY
+);
+
+export default withInstantSearch({
+  indexName: "instant_search",
+  searchClient,
+})(IndexPage);
