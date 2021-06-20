1ba6e16aef214aff2744b81569c99568e06f6034
HoSoo Lee <de24world@gmail.com>
2021-06-21 00:05:57 +0200

    update tailwind & create-next-app & hit type

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,30 +8,50 @@ import {
   RefinementList,
   SearchBox,
 } from "react-instantsearch-dom";
+// import styles from '../styles/Home.module.css'
 
-const HitComponent = ({ hit }) => {
+interface hitIData {
+  hit: indexIData;
+}
+
+interface indexIData {
+  image_url: string;
+  ratings_average: number;
+  ratings_sum: number;
+  price: number;
+}
+
+const searchClient = algoliasearch(
+  process.env.APPLICATION_ID,
+  process.env.API_KEY
+);
+
+const HitComponent = ({ hit }: hitIData) => {
   console.log(hit, "this is hit");
 
   return (
-    <div className="hit">
-      <div>
-        <div className="hit-picture">
-          <img src={`https://img.computerunivers.net${hit.image_url}`} />
-        </div>
+    <div className="hit flex">
+      <div className="hit-picture">
+        <img src={`https://img.computerunivers.net${hit.image_url}`} />
       </div>
       <div className="hit-content">
-        <div>
-          <Highlight attribute="name" hit={hit} /> <br />
-          <span> - ${hit.price}</span> <br />
-          <span> - {hit.ratings_average} stars</span> <br />
+        <div className="hit-name">
+          <Highlight attribute="name" hit={hit} />
         </div>
-        <div className="hit-type">
-          {/* <Highlight attribute="type" hit={hit} /> */}
+        <div className="hit-rating">
+          <span> {hit.ratings_average} stars</span>
+          <span> ({hit.ratings_sum}) </span>
         </div>
-        <div className="hit-description">
-          {/* <Highlight attribute="description" hit={hit} /> */}
+        <div className="hit-price">
+          <h3> {hit.price} €</h3>
         </div>
       </div>
+      {/* <div className="hit-manufacturer">
+        <Highlight attribute="manufacturerslug" hit={hit} />
+      </div> */}
+      {/* <div className="hit-description">
+        <Highlight attribute="description" hit={hit} />
+      </div> */}
     </div>
   );
 };
@@ -39,16 +59,19 @@ const HitComponent = ({ hit }) => {
 function IndexPage() {
   return (
     <>
-      <Configure hitsPerPage={12} />
+      <Configure hitsPerPage={4} />
       <header>
         <SearchBox />
       </header>
       <main>
-        <div className="menu">
-          <RefinementList attribute="categories" />
-        </div>
-        <div className="results">
-          <Hits hitComponent={HitComponent} />
+        <div className="grid grid-cols-2 gap-4">
+          <div className="menu">
+            RefinementList - menu
+            <RefinementList attribute="categories" />
+          </div>
+          <div className="results">
+            <Hits hitComponent={HitComponent} />
+          </div>
         </div>
       </main>
       <footer>
@@ -57,11 +80,6 @@ function IndexPage() {
     </>
   );
 }
-
-const searchClient = algoliasearch(
-  process.env.APPLICATION_ID,
-  process.env.API_KEY
-);
 
 export default withInstantSearch({
   indexName: "Stage-ComputerUniverse",
